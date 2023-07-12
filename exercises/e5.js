533ec1a324d62653292dc38d80be6e173f989823
m <mbddvm@gmail.com>
2023-07-12 09:18:13 -0500

    Corrected missing lesson5

--- exercises/e5.js
+++ exercises/e5.js
@@ -13,7 +13,13 @@
 
 // Your code goes here...
 
-
+function getDiffTwentySeven(num) {
+  if (num > 27) {
+    return (num - 27)*2;
+  } else {
+    return 27 - num;
+  }
+}
 
 
 /** =========================
@@ -23,7 +29,13 @@
  * Example2: sumOfTwoIntegers(12, 12) => 72
  */
 
-// Your code goes here
+function sumOfTwoIntegers(num1, num2) {
+  if (num1 === num2) {
+    return (num1+num2)*3;
+  } else {
+    return num1 + num2;
+  }
+}
 
 
 
@@ -37,7 +49,12 @@
  * Example3: isOneOfThemOrTheirSumIsFourty(23, 16) => false
  */
 
-// Your code goes here...
+function isOneOfThemOrTheirSumIsFourty(num1, num2) {
+  if (num1 === 40 || num2 ===40 || (num1 + num2) === 40) {
+    return true;
+  }
+  return false;
+}
 
 
 
